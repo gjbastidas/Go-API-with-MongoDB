820b9a1c76f417513f97a5de71706c7a310b3541
gustavo bastidas <gustavo_bastidas@mckinsey.com>
2023-01-23 19:52:32 -0600

    fixed app tests

--- app/app_test.go
+++ app/app_test.go
@@ -7,19 +7,27 @@ import (
 	"testing"
 
 	appMocks "github.com/gjbastidas/GoSimpleAPIWithMongoDB/mocks"
+	"github.com/golang/mock/gomock"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func TestCreatePost(t *testing.T) {
 	var a App
 	router := mux.NewRouter()
 	subRouter := router.PathPrefix("/post").Subrouter()
-	subRouter.HandleFunc("/", a.handleCreatePost(&appMocks.MockPost{}, "fakeDB", "fakeCollection")).Methods("POST")
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockPost := appMocks.NewMockPostIface(ctrl)
+	mockPost.EXPECT().CreatePost(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(&mongo.InsertOneResult{InsertedID: 123456}, nil).Times(1)
+
+	subRouter.HandleFunc("/", a.handleCreatePost(mockPost, "fakeDB", "fakeCollection")).Methods("POST")
 
 	w := httptest.NewRecorder()
 	json := strings.NewReader(`{"content":"updated post", "author":"gus bast"}`)
-	r, err := http.NewRequest("POST", "/posts/", json)
+	r, err := http.NewRequest("POST", "/post/", json)
 	router.ServeHTTP(w, r)
 
 	assert.NoError(t, err)
@@ -27,10 +35,17 @@ func TestCreatePost(t *testing.T) {
 }
 
 func TestGetPost(t *testing.T) {
+	// TODO
 	var a App
 	router := mux.NewRouter()
 	subRouter := router.PathPrefix("/post").Subrouter()
-	subRouter.HandleFunc("/", a.handleGetPost(&appMocks.MockPost{}, "fakeDB", "fakeCollection")).Methods("GET")
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockPost := appMocks.NewMockPostIface(ctrl)
+	mockPost.EXPECT().GetPost(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(&mongo.SingleResult{}).Times(1)
+
+	subRouter.HandleFunc("/", a.handleGetPost(mockPost, "fakeDB", "fakeCollection")).Methods("GET")
 
 	w := httptest.NewRecorder()
 	r, err := http.NewRequest("GET", "/posts/123456", nil)
